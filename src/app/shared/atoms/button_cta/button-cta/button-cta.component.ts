9bcb72cc584b487b241a2ed368bc78b210955a2a
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-02-25 16:25:37 -0300

    se crea la ruta de los links en navbar, mascotas perdidas y conocenos

---
+++ src/app/shared/atoms/button_cta/button-cta/button-cta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-cta',
+  imports: [],
+  templateUrl: './button-cta.component.html',
+  styleUrl: './button-cta.component.css'
+})
+export class ButtonCtaComponent {
+
+}
