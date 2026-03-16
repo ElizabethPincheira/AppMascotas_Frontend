0b49604916bdc64c888b694d6db5e4eb1c1dc90a
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-03-15 23:45:05 -0300

    se completa formulario y se conecta a nuevo backend, se crea usuario desde fronten

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -6,7 +6,14 @@ import axios, { Axios } from 'axios';
 })
 export class UsersService {
 
-    private apiUrl = 'http://localhost:8080/auth/register';
+    private apiUrl = 'http://localhost:3000/api/v1/user';
+
+    async register(email: string, password: string, nombre: string, comuna: string, ciudad: string): Promise<any> {
+
+        const registerData = { email, password, nombre, comuna, ciudad };
+        const response = await axios.post(this.apiUrl, registerData);
+        return response.data;
+    }
 
 }
 
@@ -17,11 +24,65 @@ export class UsersService {
 
 
 
-  /*crear usuario
+  /*
+
+
+
+   @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+
+    @IsString()
+    @IsOptional()
+    nombre?: string;
+
+    @IsString()
+    comuna: string;
+
+    @IsString()
+    ciudad: string;
+
+    @IsNumber()
+    @IsOptional()
+    reputacion?: number;
+
+    @IsDate()
+    @IsOptional()
+    fechaRegistro?: Date;
+
+    @IsString()
+    @IsOptional()
+    @IsMongoId()
+    ubicacion?: string; 
+  
+    @Post()
+  crearUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.crearUser(createUserDto);
+  }
+  
+  
+  
+  
+  
+  
+  
+  crear usuario
     @PostMapping("/register")
     @Operation(summary = "Registro de usuario", description = "Registra un nuevo usuario con email y contraseña")
     public ResponseEntity<LoginResponse> register(@Valid @RequestBody LoginRequest request) {
         LoginResponse response = loginService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
-        */
+        
+
+
+      private apiUrl = 'http://localhost:8080/mascotas/';
+
+  async getMascotas(): Promise<any[]> {
+    let url = this.apiUrl + 'todas';
+    const response = await axios.get(url);
+    return response.data;
+  }
+    */
--- src/app/features/register/pages/register-page/register-page.component.ts
+++ src/app/features/register/pages/register-page/register-page.component.ts
@@ -12,16 +12,82 @@ export class RegisterPageComponent {
 
   email: string = '';
   password: string = '';
+  nombre: string = '';
+  comuna: string = '';
+  ciudad: string = '';
 
-  constructor(private userService: UsersService) { }
+  respuesta: any;
 
-register() { const register = { email: this.email, password: this.password }; 
+  constructor(private usersService: UsersService) { }
 
-  console.log(register, 'register');}
+  async register() {
+    const register = {
+      email: this.email,
+      password: this.password,
+      nombre: this.nombre,
+      comuna: this.comuna,
+      ciudad: this.ciudad
+    };
+
+
+    console.log(register, 'usuario registrado');
+
+    this.respuesta = await this.usersService.register(this.email, this.password, this.nombre, this.comuna, this.ciudad);
+
+    console.log(this.respuesta, 'respuesta del servidor');
+  }
+
+
+
+
+
+}
+
+/*
+******************
+
+
+email, password, nombre, comuna, ciudad
+
+
+import { Component } from '@angular/core';
+import { CardMascotaComponent } from '../../../../shared/molecules/card-mascota/card-mascota.component';
+import { CommonModule } from '@angular/common';
+import { Mascota } from '../../../../shared/models/mascota.model';
+import { MascotaService } from '../../../../core/services/mascota.service';
+
+
+@Component({
+  selector: 'app-pets-page',
+  imports: [CommonModule, CardMascotaComponent],
+  templateUrl: './pets-page.component.html',
+  styleUrl: './pets-page.component.css'
+})
+export class PetsPageComponent {
+
+  trackById(index: number, mascota: Mascota) {
+    return mascota.id;
+  }
+
+  mascotas: Mascota[] = [];
+
+  imagenUrl: string = '';
+
+  constructor(private mascotaService: MascotaService) { }
+
+  async ngOnInit() {
+
+
+    this.mascotas = [...await this.mascotaService.getMascotas()];
+
+
+
+    console.log(this.mascotas, 'mascotas');
+  }
+
 
 
 }
 
 
-
-
+*/
