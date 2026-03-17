48a240971cc9e9963817168b250caa923a4c6ae1
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-03-17 00:41:23 -0300

    se arregla import de navbar

--- src/app/core/layout/header/navbar/navbar/navbar.component.ts
+++ src/app/core/layout/header/navbar/navbar/navbar.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router'; // 1. Importar desde el router
+import { RouterLink, RouterLinkActive } from '@angular/router'; 
 import { ButtonLoginComponent } from '../../../../../shared/atoms/button-login/button-login.component';
+import { AuthService } from '../../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, ButtonLoginComponent], // 2. Agregar a imports
+  imports: [RouterLink, RouterLinkActive, ButtonLoginComponent, CommonModule], 
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  user: any;
+
+  constructor(private authService: AuthService) {}
+
+
+  ngOnInit() {
+    this.user = this.authService.getUser();
+  }
+
+
 }
+
+
