2cc93af6bd47ca3d341d9761af7c71739406719e
juan ormeno <juan.pablo.ormeno.farias@gmail.com>
2026-03-18 03:19:11 -0300

    feat: añade nuevas rutas y componentes para tienda, donaciones y fauna, y mejora la navegación en la barra superior

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './core/layout/main-layout/main-layout/main-layout.component';
 import { AuthLayoutComponent } from './core/layout/auth-layout/auth-layout/auth-layout.component';
+import { authGuard } from './core/guards/auth.guard';
+import { noAuthGuard } from './core/guards/no-auth.guard';
 
 export const routes: Routes = [
   {
@@ -14,16 +16,54 @@ export const routes: Routes = [
             .then(m => m.PetsPageComponent)
       },
       {
+        path: 'perdidos',
+        loadComponent: () =>
+          import('./pages/pet/pets/pets-page.component')
+            .then(m => m.PetsPageComponent)
+      },
+      {
+        path: 'adopcion',
+        loadComponent: () =>
+          import('./pages/pet/pets/pets-page.component')
+            .then(m => m.PetsPageComponent)
+      },
+      {
         path: 'about',
         loadComponent: () =>
           import('./pages/pet/about/about-page.component')
             .then(m => m.AboutPageComponent)
+      },
+      {
+        path: 'tienda',
+        loadComponent: () =>
+          import('./pages/ecommer/tienda/tienda.component')
+            .then(m => m.TiendaComponent)
+      },
+      {
+        path: 'fauna',
+        loadComponent: () =>
+          import('./pages/pet/fauna/fauna.component')
+            .then(m => m.FaunaComponent)
+      },
+      {
+        path: 'donar',
+        loadComponent: () =>
+          import('./pages/pet/donaciones/donaciones.component')
+            .then(m => m.DonacionesComponent)
+      },
+      {
+        path: 'publicar',
+        canActivate: [authGuard],
+        loadComponent: () =>
+          import('./pages/pet/publicar/publicar.component')
+            .then(m => m.PublicarComponent)
       }
     ]
   },
   {
     path: 'register',
     component: AuthLayoutComponent,
+    canActivate: [noAuthGuard],
     children: [
       {
         path: '',
@@ -36,6 +76,7 @@ export const routes: Routes = [
   {
     path: 'login',
     component: AuthLayoutComponent,
+    canActivate: [noAuthGuard],
     children: [
       {
         path: '',
@@ -44,5 +85,9 @@ export const routes: Routes = [
             .then(m => m.LoginPageComponent)
       },
     ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
---
+++ src/app/pages/pet/fauna/fauna.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fauna',
+  imports: [],
+  templateUrl: './fauna.component.html',
+  styleUrl: './fauna.component.css'
+})
+export class FaunaComponent {
+
+}
