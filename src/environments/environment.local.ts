41368f6b3b15679ec46a25012ae6e4d48ac0b300
juan ormeno <juan.pablo.ormeno.farias@gmail.com>
2026-03-24 09:17:02 -0300

    feat: agregar clave de API de Google Maps en entornos de desarrollo y producción

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api/v1/',
-  googleMapsApiKey: '',
+  googleMapsApiKey: 'AIzaSyDG0VS6YcuKKjIsYx7BHG_JXUOD6e-nflw',
   appName: 'circuloanimal.cl',
   appVersion: '1.0.0',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.circuloanimal.cl/api/v1/',
-  googleMapsApiKey: '',
+  googleMapsApiKey: 'AIzaSyDG0VS6YcuKKjIsYx7BHG_JXUOD6e-nflw',
   appName: 'circuloanimal.cl',
   appVersion: '1.0.0',
 };
